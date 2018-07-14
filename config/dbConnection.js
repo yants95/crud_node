42daaa43c94ab9ac6cf264a7c6c692c4ae852271
Yan Soares <yts.1995@gmail.com>
2018-07-14 17:04:41 -0300

    CRUD Node

---
+++ config/dbConnection.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+var connMySQL = function() {
+	return mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "toor",
+		database: "portal_noticias"
+	});
+}
+
+module.exports = function() {
+	return connMySQL;
+}
